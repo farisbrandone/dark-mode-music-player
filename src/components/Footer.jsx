40d57e2d46ab3dc2c5f0dab572dc99185d879f58
farisbrandone <farisbrandone@yahoo.com>
2024-10-03 14:14:45 +0100

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,15 +15,19 @@ function Footer() {
 
   return (
     <div
-      className="p-0 w-full sm:max-w-[1024px] absolute bottom-0 sm:bottom-1 cursor-pointer"
+      className="p-0 w-full flex sm:max-w-[1024px] absolute bottom-0 sm:bottom-1 cursor-pointer "
       onClick={handlePullUp}
     >
-      <div className="w-full p-0 -mb-3">
-        <ChevronUp size={40} color="#666666" className="mr-auto ml-auto" />
+      <div className="w-full ">
+        <div className="w-full p-0 -mb-3">
+          <ChevronUp size={40} color="white" className="mr-auto ml-auto" />
+        </div>
+        <div className="w-full flex ">
+          <p className="text-center pl-2 pr-2 text-xs flex items-center sm:text-xl w-full opacity-[0,2832] p-0 pb-2 text-white font-bold footer-me">
+            PULL UP THE SONG LIST
+          </p>
+        </div>
       </div>
-      <p className="text-center text-xs sm:text-xl w-full opacity-[0,2832] p-0 pb-2 ">
-        PULL UP THE SONG LIST
-      </p>
     </div>
   );
 }
